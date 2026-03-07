b040b6e6fe4d6988a6488f9c2aa387b161652415
Noisy <161953152+cokguss@users.noreply.github.com>
2026-03-07 13:53:09 +0800

    Refactor routing and add LanguageProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-/**
- * @license
- * SPDX-License-Identifier: Apache-2.0
- */
-
-import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
-import Login from '@/pages/Login';
-import Register from '@/pages/Register';
-import Dashboard from '@/pages/Dashboard';
-import CoinDetail from '@/pages/CoinDetail';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import CoinDetail from './pages/CoinDetail';
+import { LanguageProvider } from './context/LanguageContext';
 
 function AnimatedRoutes() {
   const location = useLocation();
@@ -16,20 +12,24 @@ function AnimatedRoutes() {
   return (
     <AnimatePresence mode="wait">
       <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/coin/:id" element={<CoinDetail />} />
-        <Route path="/" element={<Navigate to="/login" replace />} />
       </Routes>
     </AnimatePresence>
   );
 }
 
-export default function App() {
+function App() {
   return (
-    <BrowserRouter>
-      <AnimatedRoutes />
-    </BrowserRouter>
+    <LanguageProvider>
+      <Router>
+        <AnimatedRoutes />
+      </Router>
+    </LanguageProvider>
   );
 }
+
+export default App;
